141edbc39c18ac2796471878e794ca0e59622d2a
Lucas Trampal <lucas.trampal@ensimag.grenoble-inp.fr>
2018-04-03 17:02:09 +0200

    BD instruction

--- emulator/assembly/BD.js
+++ emulator/assembly/BD.js
@@ -4,6 +4,10 @@ class BD extends Operation {
   constructor(OD, OF, bullGamma) {
     super(7, 2, OD, OF, bullGamma)
   }
+
+  execute() {
+    this.bullGamma.md = this.bullGamma.getMemory(2).blocks[this.OD];
+  }
 }
 
 module.exports.BD = BD;
---
+++ emulator/testing/assembly/instructions/BD.js
@@ -0,0 +1,19 @@
+const NB_BLOCKS_PER_MEMORY = require("../../../machine/memory").NB_BLOCKS_PER_MEMORY;
+BullGamma = require("../../../machine/bullGamma").BullGamma;
+MEMORY_MODE = require("../../../machine/bullGamma").MEMORY_MODE;
+assert = require('assert');
+BD = require("../../../assembly/BD").BD;
+Memory = require("../../../machine/memory").Memory;
+
+describe('BD', function() {
+  describe('#execute()', function () {
+    it('should transfer any M2 block to MD', function () {
+      let bullGamma = new BullGamma();
+      let m2 = bullGamma.getMemory(2);
+      let bd = new BD(2, 0, bullGamma);
+      m2.blocks[2] = 15
+      bd.execute();
+      assert.equal(bullGamma.md, 15);
+    });
+  });
+});
