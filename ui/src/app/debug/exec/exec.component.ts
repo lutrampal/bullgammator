60a8d26e4e35358a97910a45c71c1a105d4672b3
José Maillard <jose.maillard@ensimag.fr>
2018-04-04 02:39:09 +0200

    missing comma

--- ui/src/app/debug/exec/exec.component.ts
+++ ui/src/app/debug/exec/exec.component.ts
@@ -21,7 +21,7 @@ export class ExecComponent implements OnInit {
   nextInst() {
     let insts = this.s.getInstructions(3);
     insts[this.s.getLine(3)].execute();
-    this.bull.bullgamma.cp = (this.bull.bullgamma.cp + 1) % (insts.length > 64 ? 64: insts.length;
+    this.bull.bullgamma.cp = (this.bull.bullgamma.cp + 1) % (insts.length > 64 ? 64: insts.length);
   }
 
 }
