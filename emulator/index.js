070e27e11c93a710ad4cd447737aa5bcd3103700
José Maillard <jose.maillard@ensimag.fr>
2020-10-25 21:09:58 +0100

    Exported connections table

--- emulator/index.js
+++ emulator/index.js
@@ -1,5 +1,6 @@
 exports.BullGamma = require("./machine/bullGamma").BullGamma;1
 exports.constants = require("./machine/constants");
+exports.ConnectionsTable = require("./machine/connectionsTable/connectionsTable").ConnectionsTable;
 exports.Debug = require("./control/debug").Debug;
 exports.Editor = require("./control/editor").Editor;
 exports.Execution = require("./control/execution").Execution;
